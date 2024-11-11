aea17a55bbd7e79288f9320c9d4ca67b8b9d9906
EldenXLe <fategrandorderx22@gmail.com>
2024-11-11 18:05:38 -0300

    inclusão de novo projeto

--- meuportfolio/src/projetos.js
+++ meuportfolio/src/projetos.js
@@ -11,6 +11,22 @@ function Projetos() {
                 {/* Projeto 1 */}
                 <div className="hover:scale-[1.03] duration-300 md:w-[480px] border-2 border-[#2540c5] bg-gradient-to-r from-[#231d1d] to-[#9d9d9d] rounded-[15px]">
                     <div>
+                        <img src='/climaCon.png' className='border-b-2 border-[white] w-full h-[200px] md:h-[300px] rounded-tl-[15px] rounded-tr-[15px]'></img>
+                    </div>
+                    <div className="p-4">
+                        <h1 className="text-2xl font-bold">Consulta de Clima</h1>
+                        <p className="text-[15px]">Uma pagina que exibe o clima da sua região quando permitido obter a localidade e também permite pesquisar o clima de qualquer cidade do mundo!</p>
+                        <div className='p-4'>
+                            <p className='text-[15px] font-italic pb-3'>Tecnologias: ReactJS, TailwindCSS, fetch API</p>
+                            <a href="https://consulta-clima-ten.vercel.app" target="_blank" rel="noopener noreferrer" className='p-2'><MdRocketLaunch className="inline size-[25px]" /></a>
+                            <a href="https://github.com/EldenXLe/Consulta-Clima" target="_blank" rel="noopener noreferrer"><FaGithub className="inline size-[25px]" /></a>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Projeto 2 */}
+                <div className="hover:scale-[1.03] duration-300 md:w-[480px] border-2 border-[#2540c5] bg-gradient-to-r from-[#231d1d] to-[#9d9d9d] rounded-[15px]">
+                    <div>
                         <img src='/wallpaperGallery.png' className='border-b-2 border-[white] w-full h-[200px] md:h-[300px] rounded-tl-[15px] rounded-tr-[15px]'></img>
                     </div>
                     <div className="p-4">
@@ -24,7 +40,7 @@ function Projetos() {
                     </div>
                 </div>
 
-                {/* Projeto 2 */}
+                {/* Projeto 3 */}
                 <div className="hover:scale-[1.03] duration-300 md:w-[480px] border-2 border-[#2540c5] bg-gradient-to-r from-[#231d1d] to-[#9d9d9d] rounded-[15px]">
                     <div>
                         <img src='/organoGame.png' className='border-b-2 border-[white] w-full h-[200px] md:h-[300px] rounded-tl-[15px] rounded-tr-[15px]'></img>
